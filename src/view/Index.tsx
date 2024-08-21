4e8be086ac65692a5d6382fe02f6394800f16d12
Barbara Fernandes <barbarafernandes61@gmail.com>
2024-08-21 17:52:24 -0300

    first commit

---
+++ src/view/Index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index: React.FC = () =>{
+    return(
+        <div className="index">
+            hello world!
+        </div>
+    );
+};
+
+export default Index;
